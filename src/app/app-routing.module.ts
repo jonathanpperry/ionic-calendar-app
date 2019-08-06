1bf51cded442512aa1556809c67e200dd2507aa4
Jonny Ramen <jpper@umich.edu>
2019-08-06 18:34:24 +0900

    Ran cordova build for android and generated event modal page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'event-modal', loadChildren: './event-modal/event-modal.module#EventModalPageModule' },
 ];
 
 @NgModule({
